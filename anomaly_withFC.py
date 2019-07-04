1ae8a034cbeb27c15d086f1d4a0cbfc0c5a2349c
kentaroy47 <kentaroy@stanford.edu>
2019-07-04 00:10:01 +0000

    fix FC scripts. added comments.

--- anomaly_withFC.py
+++ anomaly_withFC.py
@@ -39,6 +39,8 @@ D=args.window_size #the size of the window width
 batchsize = args.batchsize   # minibatch size
 epoch=args.epoch
 
+###### data preparation #####
+
 # load
 x_train_data=np.loadtxt(train_name,delimiter=',')
 x_test_data=np.loadtxt(test_name,delimiter=',')
@@ -47,12 +49,26 @@ x_test_data=np.loadtxt(test_name,delimiter=',')
 x_train_data = x_train_data/(np.std(x_train_data))
 x_test_data = x_test_data/(np.std(x_test_data))
 
-#split
-x_train_data=x_train_data.reshape([x_train_data.shape[0],x_train_data.size/x_train_data.shape[0]])
-x_test_data=x_test_data.reshape([x_test_data.shape[0],x_test_data.size/x_test_data.shape[0]])
+print("x_train data", x_train_data.shape)
+print("x_test data", x_test_data.shape)
 
-Split_train_data=x_train_data.reshape([x_train_data.shape[0]/D,x_train_data.shape[1]*D])
-Split_test_data=x_test_data.reshape([x_test_data.shape[0]/D,x_test_data.shape[1]*D])
+"""
+split train data and test data into D length sequences.
+the keras model will try to predict the *next* D length sequence.
+
+if the model results and the real data has no contradictions, the state is non-anomaly (or nomal)
+if the model results and the real data have large differences, it is likely to be an anomaly state.
+
+# 2019/07/04 fixed to remove divide errors.
+"""
+x_train_data = x_train_data.reshape([x_train_data.shape[0], 1]) #x_train_data.size/x_train_data.shape[0]])
+x_test_data = x_test_data.reshape([x_test_data.shape[0], 1]) #x_test_data.size/x_test_data.shape[0]])
+
+print("x_train data", x_train_data.shape)
+print("x_test data", x_test_data.shape)
+
+Split_train_data = x_train_data.reshape([int(x_train_data.shape[0]/D), x_train_data.shape[1]*D])
+Split_test_data=x_test_data.reshape([int(x_test_data.shape[0]/D), x_test_data.shape[1]*D])
 
 Split_train_data_x=Split_train_data[0:-1,:]
 Split_train_data_y=Split_train_data[1::,:]
@@ -68,7 +84,7 @@ Split_train_data_y = Split_train_data.reshape(Split_train_data.shape[0], img_row
 
 input_shape = (img_rows, 1, 1)
 
-#Model Net
+##### Model Setup #####
 #size of parameters
 batch_size = 64
 D=400
@@ -113,6 +129,7 @@ model.compile(loss=keras.losses.mean_squared_error, optimizer='rmsprop')
 
 model.summary()
 
+### Model Training ###
 history = model.fit(Split_train_data, Split_train_data_y,
           batch_size=batch_size,
           epochs=epoch,
@@ -134,7 +151,7 @@ for x in range(Loss_keras.size-mean_window):
 #normalize
 Loss_keras_processed = Loss_keras_processed/(np.std(Loss_keras_processed))
     
-# plot results
+##### plot results #####
 fig1 = plt.figure()
 plt.xlabel("sample")
 plt.ylabel("anomal score")
